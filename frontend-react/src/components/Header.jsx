15944d83cf9c8173e766ca248554f1688bb5f334
nimbareharsh42 <nimbareharsh6@gmail.com>
2025-11-22 17:22:44 +0530

    homepage react

---
+++ frontend-react/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from './Button.jsx'
+
+const Header = () => {
+  return (
+    <>
+      <nav className = 'navbar container pt-3 pd-3 align-items-start'>
+
+        <a className = 'navbar-brand text-light 'href="">Stock Prediction Portal</a>
+
+        <div>
+          <Button text = 'Login' class = 'btn-outline-info' />
+          &nsbn;
+          <Button text = 'Register' class = 'btn-info' />
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
