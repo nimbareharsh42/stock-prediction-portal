cd84c18affd65f860466e4eac57358e686f9f43c
nimbareharsh42 <nimbareharsh6@gmail.com>
2025-11-23 21:36:51 +0530

    registration backend/api

--- backend-drf/stock_prediction_portal/settings.py
+++ backend-drf/stock_prediction_portal/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4nz4ue5klz!!u-)ox5pf@z-g#-no_mkd2ixt=b6ivq-y)$3vo@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default = False, cast = bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    "accounts",
+    "api",
 ]
 
 MIDDLEWARE = [
